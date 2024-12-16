f20fa0be71ad09b9426123870b08c04ed24dccda
anna <141744002+xneqweg@users.noreply.github.com>
2024-12-16 11:08:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ elif (month == 6 and day >= 1) or (month == 7) or (month == 8) or (month == 8 an
     season = "Лето"
 elif (month == 9 and day >= 1) or (month == 10) or (month == 11 and day <= 30):
     season = "Осень"
-else:
+elif (month == 12 and day >= 1 and day <= 31) or (month == 1 and day >= 1 and day <= 31 ) or (month == 2 and day >= 1 and day <= 29 ):
     season = "Зима"
-
+else: 
+    print("неправельный ввод")
 print(f"Дата: {day}.{month} относится к сезону: {season}")
